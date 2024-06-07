c8bd7e1d893627070aed25b1d2be6ff0575f5946
Edson Rodrigues <edson091990@gmail.com>
2024-06-07 03:04:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const {createServer} = require('node:http')
+
+var http = require('http')
+var fs = require('fs')
+http.createServer(function(req, res) {
+  fs.readFile('./index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    return res.end()
+  })
+}).listen(3000)
